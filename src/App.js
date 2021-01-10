5f55ea8f8a7ea864ae0b00908853a29d7c9dfc53
FruzsinaElekes <elekes.fr@gmail.com>
2021-01-10 17:44:10 +0100

    Add Router with routes to App.js

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from 'react';
+import {Route, BrowserRouter as Router} from 'react-router-dom';
+import Favourites from './components/Favourites';
+import Navbar from './components/Navbar';
+
 
 function App() {
+  //fetch list of symbols (limit 200?)
+
+
   return (
+    <Router>
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React..
-        </a>
-      </header>
+      <Navbar></Navbar>
+      {/* <ListMain></ListMain> */}
+      <Route path="/favourites" component={Favourites}></Route>
+      <Route exact path="/" ></Route>
+      {/* <Route path="/stock/:quote" component={DetailedView}></Route> */}
     </div>
+    </Router>
   );
 }
 
