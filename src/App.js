553d0ec649ae2364f30960ef3c02b19d3a399bc6
FruzsinaElekes <elekes.fr@gmail.com>
2021-01-11 14:54:38 +0100

    Add example axios call using variable in .env

--- src/App.js
+++ src/App.js
@@ -2,10 +2,13 @@ import React, {useState} from 'react';
 import {Route, BrowserRouter as Router} from 'react-router-dom';
 import Favourites from './components/Favourites';
 import Navbar from './components/Navbar';
+import axios from 'axios';
 
 
 function App() {
   //fetch list of symbols (limit 200?)
+  axios.get(`https://cloud.iexapis.com/stable/stock/AAPL/company?token=${process.env.REACT_APP_IEX_API_KEY}`)
+  .then(res => console.log(res.data))
 
 
   return (
