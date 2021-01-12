86d1f1c47508511cef87640348e8d204f9595414
FruzsinaElekes <elekes.fr@gmail.com>
2021-01-12 18:35:33 +0100

    Get news for symbol from api/cache, render all NewsItems

--- src/components/detailedView/News.js
+++ src/components/detailedView/News.js
@@ -1,9 +1,30 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import NewsItem from './NewsItem';
 
-export default function News() {
+export default function News(props) {
+    console.log(props.data.symbol + " printed from news")
+
+    const [newsState, setNewsState] = useState([])
+
+    useEffect(() => {
+        if (props.data.symbol && !sessionStorage.getItem(props.data.symbol + "_news")){
+            axios.get(`https://cloud.iexapis.com/stable/stock/${props.data.symbol}/news/last/10?token=${process.env.REACT_APP_IEX_API_KEY}`)
+            .then(res => {
+                const filtered = res.data.filter(news => news.lang==="en")
+                setNewsState(filtered)
+                sessionStorage.setItem(props.data.symbol + "_news", JSON.stringify(filtered))
+                console.log(filtered)
+                })
+        } else {
+            setNewsState(JSON.parse(sessionStorage.getItem(props.data.symbol + "_news")))
+        }
+    }, [props.data.symbol])
+
     return (
         <div>
-            
+            {newsState? newsState.map(n => <NewsItem symbol= {props.data.symbol} news = {n}></NewsItem>)
+            : "Loading"}
         </div>
     )
 }
