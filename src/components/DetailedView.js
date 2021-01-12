1ddfc55f98429488f40509ce3aea3492c6009c3c
FruzsinaElekes <elekes.fr@gmail.com>
2021-01-12 16:55:42 +0100

    Pass symbol data from DetailedView to News based on url

--- src/components/DetailedView.js
+++ src/components/DetailedView.js
@@ -1,16 +1,30 @@
-import React from 'react';
+import React, { useState, useEffect, useMemo } from 'react';
 import StockData from './detailedView/StockData';
 import Video from './detailedView/Video';
 import News from './detailedView/News';
 import Graph from './detailedView/Graph';
 
 export default function DetailedView() {
+    const [details, setDetails] = useState({});
+    const url = window.location.href;
+    const symbol = url.slice(url.lastIndexOf('/')+1, url.length);
+    
+    //TODO: when switching between stocks it renders the previous stock page again
+    // would a Context solve this?
+    useEffect(() => {
+        setDetails(JSON.parse(sessionStorage.getItem(symbol)))
+    }, [symbol])
+
     return (
         <div>
+            {details &&
+            <React.Fragment>
             <StockData></StockData>
             <Graph></Graph>
             <Video></Video>
-            <News></News>
+            <News data={details}></News>
+            </React.Fragment>
+            }
         </div>
     )
 }
