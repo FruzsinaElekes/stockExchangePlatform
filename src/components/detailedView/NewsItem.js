46770805ad9b0dc267fac603c5e2f500b6d48b90
FruzsinaElekes <elekes.fr@gmail.com>
2021-01-12 21:13:57 +0100

    Add styling (positioning) to NewsItems

--- src/components/detailedView/NewsItem.js
+++ src/components/detailedView/NewsItem.js
@@ -3,15 +3,19 @@ import styled from 'styled-components';
 
 export default function NewsItem(props) {
     return (
-        <div>
-            <a href={props.news.url}>{props.news.headline}</a>
+        <div>            
             <NewsSection>
-                <img alt={props.symbol} src={props.news.image} width="300px"></img>
-                <div>
-                    <div>Source: {props.news.source}</div>
-                    <div>Date: {props.news.datetime}</div>
-                    <div>{props.news.summary}</div>
-                </div> 
+                <ImageContainer>
+                    <img alt={props.symbol} src={props.news.image} width="100%"></img>
+                </ImageContainer>
+                <NewsContent>
+                    <Title href={props.news.url}>{props.news.headline}</Title>
+                    <SourceInfo>
+                        <div>By {props.news.source}</div>
+                        <div>{new Date(props.news.datetime).toLocaleString()}</div>
+                    </SourceInfo>
+                    <p>{props.news.summary}</p>
+                </NewsContent> 
             </NewsSection>
             
             
@@ -22,5 +26,31 @@ export default function NewsItem(props) {
 
 
 const NewsSection = styled.div`
-    display: flex
+    display: flex;
+    justify-content: space-evenly;
+    margin-top: 4em
     `
+
+const NewsContent = styled.div`
+    width: 50%;
+
+`
+const ImageContainer = styled.div`
+    width: 25%;
+`
+
+const SourceInfo = styled.div`
+    margin: 0.5em 0;
+    font-style: italic
+`
+
+const Title = styled.a`
+    text-decoration: none;
+    font-size: 1.5em;
+    font-weight: bolder;
+    font-family: Georgia, 'Times New Roman', Times, serif;
+    color: black;
+    &:hover{
+        color:blue
+    }
+`
