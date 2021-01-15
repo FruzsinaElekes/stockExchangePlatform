6fac38f69bccad884a8ec0d8cc5e84c659fd44fd
FruzsinaElekes <elekes.fr@gmail.com>
2021-01-15 11:16:40 +0100

    Display correct video when moving between stocks in ListMain

--- src/components/detailedView/Video.js
+++ src/components/detailedView/Video.js
@@ -4,27 +4,26 @@ import ReactPlayer from 'react-player';
 import styled from 'styled-components';
 
 export default function Video(props) {
-
-    const [videos, setVideos] = useState(null)    
+    const [videos, setVideos] = useState([])
     
     useEffect(()=>{
-        let cachedVideos = localStorage.getItem(props.symbol + "_videos")
-        if (cachedVideos){
-            setVideos(JSON.parse(cachedVideos))
-        } else {
+        // if moving from one detailed view directly to another the previous state is available
+        // clicking back and forth between e.g twtr and tsla, we use the same video lists
+        if (videos.filter(v => v.symbol===props.symbol).length === 0){
             axios.get(`https://youtube.googleapis.com/youtube/v3/search?maxResults=25&q=${props.symbol}&key=${process.env.REACT_APP_YOUTUBE_API_KEY}`)
             .then(res => {
-                localStorage.setItem(props.symbol + "_videos", JSON.stringify(res.data))
-                setVideos(res.data)
+                console.log("fetching videos")
+                //sessionStorage.setItem("videos", JSON.stringify([...videos, {...res.data, symbol: props.symbol, timeStamp : Date.now()}]))
+                setVideos(prev => [...prev, {...res.data, symbol: props.symbol, timeStamp : Date.now()}])
             })
         }
-    }, [])
+    }, [props.symbol])
 
     return (
         <React.Fragment>
-            {videos &&
-            <CustomReactPlayer url={`https://www.youtube.com/watch?v=${videos.items[Math.floor(Math.random()*videos.items.length)].id.videoId}`} />
-            }
+            {(videos.filter(v => v.symbol===props.symbol).length > 0) ? 
+            <CustomReactPlayer url={`https://www.youtube.com/watch?v=${videos.filter(v => v.symbol===props.symbol)[0].items[Math.floor(Math.random()*25)].id.videoId}`} />
+            : <div>Loading video...</div>}
         </React.Fragment>
     )
 }
