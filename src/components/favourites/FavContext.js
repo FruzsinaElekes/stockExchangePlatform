351a1e86fa8f6c4f7c07440cf520c9c77db804e4
FruzsinaElekes <elekes.fr@gmail.com>
2021-01-21 12:57:10 +0100

    Retain favourites in localStorage

--- src/components/favourites/FavContext.js
+++ src/components/favourites/FavContext.js
@@ -3,15 +3,21 @@ import React, { createContext, useState } from 'react';
 export const FavContext = createContext();
 
 export const FavProvider = (props) => {
-    const [favourites, setFavourites] = useState([])
+    const [favourites, setFavourites] = useState(localStorage.getItem("favourites")? JSON.parse(localStorage.getItem("favourites")) : [])
 
     const addToFav = (stockData) => {
-        setFavourites(prev => [...prev, stockData])
+        setFavourites(prev => {
+            let updated = [...prev, stockData]
+            localStorage.setItem("favourites", JSON.stringify(updated))
+            return updated
+        })
+        
     }
 
     const removeFromFav = (stockData) => {
         const filteredFav = favourites.filter(f => f.symbol !== stockData.symbol)
         setFavourites(filteredFav)
+        localStorage.setItem("favourites", JSON.stringify(filteredFav))
     }
 
     return (
