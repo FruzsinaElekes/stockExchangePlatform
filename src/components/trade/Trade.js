bd3e7df50475142d3463f67d1797ef00084a4aba
FruzsinaElekes <elekes.fr@gmail.com>
2021-02-01 12:04:39 +0100

    Add form, handleChange & Submission - CORS problem

--- src/components/trade/Trade.js
+++ src/components/trade/Trade.js
@@ -1,28 +1,78 @@
 
+import axios from 'axios';
 import React, { useState } from 'react'
 
 export default function Trade(props) {
 
     const [symbol , setSymbol] = useState(props.match.params.symbol ? props.match.params.symbol : "");
+    const [limitPrice, setLimitPrice] = useState(0);
+    const [direction, setDirection] = useState("-");
+    const [count, setCount] = useState(0)
 
     const handleSubmit = () => {
-        console.log("send post request to backend, with body of form info");
+        axios.defaults.headers.post['Content-Type'] = 'application/json;charset=utf-8';
+        axios.defaults.headers.post['Access-Control-Allow-Origin'] = '*';
+        // TODO: ask for confirmation!
+
+        const body = {
+            user: 285,
+            limitPrice: limitPrice,
+            direction: direction,
+            status: "PENDING",
+            count: count,
+            date: Date.now()
+        }
+        axios.post("localhost:8080/trade", JSON.stringify(body))
+        .then(response => console.log(response))
+        .catch(error => console.log(error))
     }
 
     const handleChange = (e) => {
-        setSymbol(e.target.value);
+        const data = e.target.value;
+        switch (e.target.name){
+            case "price":
+                setLimitPrice(data);
+                break;
+            case "symbol":
+                setSymbol(data);
+                break;
+            case "direction":
+                setDirection(data);
+                break;
+            case "count":
+                setCount(data);
+                break;
+        }
     }
 
     return (
         <div>
-            This is the trading page!
+            Place an order by filling in the form below.
 
             <form onSubmit={handleSubmit}>
-                <label>
+                <div><label>
                     Symbol: 
-                    <input type="text" value={symbol} onChange={handleChange}></input>
-                </label>
-                <input type="submit" value="Submit" />
+                    <input name="symbol" type="text" value={symbol} onChange={handleChange}></input>
+                </label></div>
+                <div><label>
+                    Trade Direction: 
+                    <select name="direction" value={direction} onChange={handleChange}>
+                        <option value="SELL">SELL</option>
+                        <option value="BUY">BUY</option>
+                        <option value="-">---</option>
+                    </select>
+                </label></div>
+                <div><label>
+                    Stock Count: 
+                    <input name="count" type="number" value={count} onChange={handleChange}></input>
+                </label></div>
+                <div><label>
+                    Limit Price: 
+                    <input name="price" type="number" value={limitPrice} onChange={handleChange}></input>
+                </label></div>
+
+
+                <button onClick={handleSubmit}>Trade!</button>
 
             </form>
 
