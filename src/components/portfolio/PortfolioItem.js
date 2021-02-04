2a11929292d00fb0abaf860f036af59ded1dbce7
FruzsinaElekes <elekes.fr@gmail.com>
2021-02-04 10:11:30 +0100

    Delete Link from PortfolioItem

--- src/components/portfolio/PortfolioItem.js
+++ src/components/portfolio/PortfolioItem.js
@@ -1,8 +1,6 @@
 import React, { useState } from 'react';
-// import {Link} from 'react-router-dom';
 import Modal from '@material-ui/core/Modal';
 import History from './History';
-// import { makeStyles } from '@material-ui/core/styles';
 
 
 export default function PortfolioItem(props) {
@@ -20,7 +18,6 @@ export default function PortfolioItem(props) {
         <div>
             <p>Symbol: {props.item.symbol}</p>
             <p>Amount: {props.item.amount}</p>
-            {/* <button><Link to={`/history/${props.item.symbol}`}>History</Link></button> */}
             <button onClick={handleOpen}>History</button>
 
             <Modal open={open} onClose={handleClose}>
