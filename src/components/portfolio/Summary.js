f0fc6971e0aac7c517e5b309bcef935778dc40f7
Adam Gyore <adam.gyore@gmail.com>
2021-02-05 20:01:41 +0100

    Add update for portfolio value

--- src/components/portfolio/Summary.js
+++ src/components/portfolio/Summary.js
@@ -1,12 +1,54 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import { StockDataContext } from '../StockDataContext';
+import styled from 'styled-components';
 
 export default function Summary(props) {
+
+    const stockData = useContext(StockDataContext)[1]
+    const accountBalance = props.user.account.balance
+    let stockValue
+    
+    const getStockPrice = (symbol) => {
+        const [stock] = stockData.filter(s => s.symbol == symbol)
+        return stock.latestPrice
+    }
+
+    if (stockData.length !== 0) {
+        stockValue = props.user.portfolio.map(item => item.amount * getStockPrice(item.symbol)).reduce((a,b) => a + b, 0)
+    }
+
     return (
-        <div>
-            <p>Name: {props.user.firstName} {props.user.lastName}</p>
-            <p>Balance: {props.user.account.balance} {props.user.account.currency}</p>
-            <p>Stock: y USD</p>
-            <p>Total worth: x + y USD</p>
-        </div>
+        <Table>
+            <tbody>
+                <StyledTr><TdLeft>Name: </TdLeft><TdRight>{props.user.firstName} {props.user.lastName}</TdRight></StyledTr>
+                <StyledTr><TdLeft>Balance: </TdLeft><TdRight>{accountBalance} {props.user.account.currency}</TdRight></StyledTr>
+                {stockValue && <>
+                <StyledTr><TdLeft>Stock: </TdLeft><TdRight>{stockValue} USD</TdRight></StyledTr>
+                <StyledTr><TdLeft>Total worth: </TdLeft><TdRight>{stockValue + accountBalance} USD</TdRight></StyledTr>
+                </>}
+            </tbody>
+        </Table>
     )
 }
+
+
+const TdLeft = styled.td`
+    border-bottom: 1px solid #ddd;
+    padding: 10px;
+    text-align: left;
+`
+
+const TdRight = styled.td`
+    border-bottom: 1px solid #ddd;
+    padding: 10px;
+    text-align: right;
+`
+const StyledTr = styled.tr`
+`
+const Table = styled.table`
+    border-spacing: 20px;
+    @media (max-width: 768px){
+        font-size: 1.2em;
+        margin-left: auto;
+    }
+`
