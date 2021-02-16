1326c56fa1c3f965dc8e9af08fd1f1e0e2235f19
Adam Gyore <adam.gyore@gmail.com>
2021-02-16 17:13:31 +0100

    Add error handling for login request

--- src/components/authentication/LoginForm.js
+++ src/components/authentication/LoginForm.js
@@ -2,6 +2,7 @@ import React, { useContext, useState, useRef, Fragment } from 'react';
 import { Redirect } from 'react-router-dom';
 import { UserContext } from '../UserContext';
 import { TextField, Button, Checkbox, Typography, FormControlLabel } from "@material-ui/core";
+import Modal from '@material-ui/core/Modal';
 import styled from 'styled-components';
 
 
@@ -9,9 +10,12 @@ export default function LoginForm(props) {
     let [userData, setUserData] = useContext(UserContext)
     const [isLoading, setIsLoading] = useState(false);
     const [redirect, setRedirect] = useState(false);
-    const [isPasswordVisible, setIsPasswordVisible] = useState(false);
+    const [open, setOpen] = useState(false);
+    const [error, setError] = useState("");
     const loginEmail = useRef();
     const loginPassword = useRef();
+    const errorOpen = () => setOpen(true);
+    const errorClose = () => setOpen(false);
 
     const handleLogin = () => {
         const body = {
@@ -23,6 +27,7 @@ export default function LoginForm(props) {
                 body: JSON.stringify(body),
                 headers: {'Content-Type': 'application/json;charset=utf-8'}
             })
+            .then(handleBadRequest)
             .then(resp => resp.json())
             .then(response => {
                 setUserData({
@@ -32,12 +37,27 @@ export default function LoginForm(props) {
                 document.cookie = `access_token=${response.token}`
                 setRedirect(true)
             })
-            .catch(error => console.log(error))
+            .catch(err => console.log(err))
+    }
+
+    const handleBadRequest = (response) => {
+        if (!response.ok) {
+            if (response.status === 403) {
+                setError("Invalid username or password!")
+                errorOpen()
+                throw Error(error)
+            }
+            throw Error(response.statusText)
+        }
+        else {
+            return response
+        }
     }
 
     return(
     <StyledDiv>
-        {redirect === true ? <Redirect to="/" /> : <></>} 
+        {redirect === true ? <Redirect to="/" /> : <></>}
+        <Modal open={open} onClose={errorClose}><ModalContent>{error}</ModalContent></Modal>
         <Fragment>
         <TextField
             variant="outlined"
@@ -57,6 +77,7 @@ export default function LoginForm(props) {
             required
             fullWidth
             label="Password"
+            type="password"
             inputRef={loginPassword}
             autoComplete="off"
             onChange={() => {
@@ -98,4 +119,13 @@ export default function LoginForm(props) {
 const StyledDiv = styled.div`
     margin: auto auto;
     width: 400px;
+`
+
+const ModalContent = styled.div`
+    padding: 2em;
+    background-color:white;
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
 `
