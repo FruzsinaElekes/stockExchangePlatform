367319c7998891f7ab40a2d6d364c06d15546777
Adam Gyore <adam.gyore@gmail.com>
2021-02-21 20:39:31 +0100

    Add firs version of ws data update

--- src/components/StockDataContext.js
+++ src/components/StockDataContext.js
@@ -1,4 +1,5 @@
 import React, { createContext, useState, useEffect } from 'react';
+import SockJsClient from 'react-stomp';
 import axios from 'axios';
 
 export const StockDataContext = createContext();
@@ -6,33 +7,13 @@ export const StockDataContext = createContext();
 export const StockDataProvider = (props) => {
     const [stockList, setStockList] = useState([]);
     const [stockData, setStockData] = useState([]);
+    const [sockJsclient, setClient] = useState()
     const stockBaseRoute = process.env.REACT_APP_ORIGIN + process.env.REACT_APP_STOCK_BASE_ROUTE
-    const stockChangeRoute = process.env.REACT_APP_ORIGIN + process.env.REACT_APP_STOCK_CHANGE_ROUTE
 
     useEffect(() => {
         axios.get(stockBaseRoute)
         .then(data => setStockList(data.data))
     }, [])
-
-    useEffect(() => {
-        fetchAll(stockList)
-        const interval = setInterval(() => {
-            fetchAll(stockList)
-        }, 5000)
-        return () => clearInterval(interval);
-    }, [stockList])
-
-
-    async function fetchAll (stockList) {
-        const data = await Promise.all(stockList.map(stock => fetchStockData(stock.symbol)))
-        setStockData(data)
-    }
-
-    async function fetchStockData (symbol) {
-        const symbolData = await axios.get(stockChangeRoute + symbol)
-        return symbolData.data
-    } 
-
 
     function getStock(symbol) {
         const [stock] = stockData.filter(s => s.symbol === symbol)
@@ -46,6 +27,9 @@ export const StockDataProvider = (props) => {
 
     return (
         <StockDataContext.Provider value={[getStockName, stockData, getStock, stockList.map(s => s.symbol)]}>
+        <SockJsClient url='http://localhost:8080/socket' topics={['/stock-data']}
+          onMessage={(data) => { setStockData(data); console.log(data)}}
+          ref={ (client) => { setClient(client) }} />
             {props.children}
         </StockDataContext.Provider>
     )
