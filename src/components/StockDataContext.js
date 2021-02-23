d8187f0e04517a565691db5e1d32ab84720c6c1a
Adam Gyore <adam.gyore@gmail.com>
2021-02-23 14:42:26 +0100

    Remove react-stomp, add stockjs to package and update .env

--- src/components/StockDataContext.js
+++ src/components/StockDataContext.js
@@ -1,23 +1,46 @@
 import React, { createContext, useState, useEffect } from 'react';
-import SockJsClient from 'react-stomp';
+import { Client, Message } from '@stomp/stompjs';
 import axios from 'axios';
 
-export const StockDataContext = createContext();
+export const StockDataContext = createContext()
 
 export const StockDataProvider = (props) => {
-    const [stockList, setStockList] = useState([]);
-    const [stockData, setStockData] = useState([]);
-    const [sockJsclient, setClient] = useState()
+    const [stockList, setStockList] = useState([])
+    const [stockData, setStockData] = useState([])
+    const [stomClient, setStompClient] = useState()
     const stockBaseRoute = process.env.REACT_APP_ORIGIN + process.env.REACT_APP_STOCK_BASE_ROUTE
     const websocketRoute = process.env.REACT_APP_WEBSOCKET_ROUTE
     const stockListTopic = process.env.REACT_APP_STOMP_STOCKLIST
     const stockDataTopic = process.env.REACT_APP_STOMP_STOCKDATA
 
-
     useEffect(() => {
         axios.get(stockBaseRoute)
         .then(data => setStockList(data.data))
     }, [])
+
+    useEffect(() => {
+        const client = new Client()
+        client.brokerURL = websocketRoute
+        client.onConnect = frame => {
+            client.subscribe(stockListTopic, onMessageHandler)
+            client.subscribe(stockDataTopic, onMessageHandler)
+        }
+        client.activate()
+        setStompClient(client)
+        return () => client.deactivate()
+    }, [])
+
+    function onMessageHandler(message) {
+        const destination = message.headers.destination
+        const data = JSON.parse(message.body)
+        if (destination === stockListTopic) {
+            setStockData(data)
+        }
+        if (destination === stockDataTopic) {
+            setStockData(prevState => prevState.map(
+                element => element.symbol === data.symbol ? data : element))
+        }
+    }
 
     function getStock(symbol) {
         const [stock] = stockData.filter(s => s.symbol === symbol)
@@ -28,21 +51,9 @@ export const StockDataProvider = (props) => {
         const [stock] = stockList.filter(s => s.symbol === symbol)
         return stock && stock.companyName
     }
-
-    function onMessageHandler(data, topic) {
-        if (topic === stockListTopic) {
-            setStockData(data)
-        }
-        if (topic === stockDataTopic) {
-            setStockData(prevState => prevState.map(
-                element => element.symbol === data.symbol ? data : element))
-        }
-    }
-
+    
     return (
         <StockDataContext.Provider value={[getStockName, stockData, getStock, stockList.map(s => s.symbol)]}>
-        <SockJsClient url={ websocketRoute } topics={[stockListTopic, stockDataTopic]}
-          onMessage={ onMessageHandler } ref={ (client) => { setClient(client) }} />
             {props.children}
         </StockDataContext.Provider>
     )
